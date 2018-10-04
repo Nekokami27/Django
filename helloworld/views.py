8aacaf33a4766ce6782b9585a78216bdf17e36bb
nekokami0227 <terryhappy510227@gmail.com>
2018-10-04 09:53:51 +0800

    edited

--- helloworld/views.py
+++ helloworld/views.py
@@ -6,4 +6,18 @@ from django.contrib.auth.models import User
 
 
 def index(request):
-	return render(request, "nekokami.html")
+	#template = loader.get_template("nekokami.html")
+	import random
+	list20 = []
+	i = 20
+	while i>0:
+		pics = str(random.randint(1,1088))
+		list20.append("https://picsum.photos/200/200/?image="+pics)
+		i-=1
+    
+	return render(request, "nekokami.html" ,{'list20':list20})
+
+#def results(request):
+#	guess = request.GUESS.get('msg')
+#	result = 1
+#	return HttpResponse(render(guess,result))
