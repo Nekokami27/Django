95bd63caa90920acecc7b06f366342b19fdf6d97
nekokami0227 <terryhappy510227@gmail.com>
2018-10-05 12:11:38 +0800

    edited

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    # path('', views.results, name='results') 
+
 ]
--- helloworld/views.py
+++ helloworld/views.py
@@ -3,19 +3,35 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from guestbook.models import TextMessage
 
 
 def index(request):
 	#template = loader.get_template("nekokami.html")
-	import random
-	list20 = []
-	i = 20
-	while i>0:
-		pics = str(random.randint(1,1088))
-		list20.append("https://picsum.photos/200/200/?image="+pics)
-		i-=1
     
-	return render(request, "nekokami.html" ,{'list20':list20})
+	
+	t1 = TextMessage.objects.create(talker='Michael' , message= 'How is everybody doing?')
+	t2 = TextMessage.objects.create(talker='Alex' , message= 'I fine.')
+	t3 = TextMessage.objects.create(talker='Chard' , message= 'I fine too.')
+
+	msgs = TextMessage.objects.all()
+
+
+
+	return render(request, "nekokami.html" ,locals())
+
+#def index(request):
+	#template = loader.get_template("nekokami.html")
+#	import random
+#	list20 = []
+#	i = 24
+#	while i>0:
+#		pics = str(random.randint(1,1088))
+##		list20.append("https://picsum.photos/200/200/?image="+pics)
+#		i-=1
+    
+
+#	return render(request, "nekokami.html" ,{'list20':list20})
 
 #def results(request):
 #	guess = request.GUESS.get('msg')
